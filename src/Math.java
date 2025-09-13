421ac0aea7628b3f41c9d51816210f6b212ba38a
abshir <abshir88810@gmail.com>
2025-09-13 20:35:22 +0300

    firt connit

---
+++ src/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+    int x ;
+    int y;
+    int z;
+
+    public Math(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public  void  add()  {
+        z= x+y;
+       System.out.println(z);
+    }
+}
